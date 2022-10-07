959a95c1a41bad38baf7b4f2dc00ae313cbfbc27
Nishit <nishitranjan41@gmail.com>
2022-10-07 17:33:06 +0530

    AssignmentProgramming Of Day-38

--- BasicsProgramming.js
+++ BasicsProgramming.js
@@ -2,5 +2,7 @@ let a = 10;// sequence pratice program
 let b = 20;
 let c = a + b;
 console.log(c);
-let x = Math.floor(Math.random()*10);
+let x = Math.floor(Math.random()*10);//RandomFunctiionTogetSingleDigitNumber
 console.log(x);
+let y = Math.floor(Math.random()*10%6+1);
+console.log(y);
