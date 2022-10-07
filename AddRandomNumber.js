8e625ee6b330aa9f04f7cff1fc37ac15ea987ca1
Nishit <nishitranjan41@gmail.com>
2022-10-07 18:21:12 +0530

    AddRandomNumber.js

---
+++ AddRandomNumber.js
@@ -0,0 +1,17 @@
+let a = 10;// sequence pratice program
+let b = 20;
+let c = a + b;
+let add = 0;
+let i = 0;
+console.log(c);
+while(i <= 5){
+var x = Math.floor(Math.random()*100);//RandomFunctiionTogetSingleDigitNumber
+console.log(x);
+var y = Math.floor(Math.random()*100);
+console.log(y);
+let sum = x+y;
+add = add + sum;
+i++;
+}
+console.log(add);
+
