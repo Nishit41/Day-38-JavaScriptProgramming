6781f68561018d59327b8ed8b3f2c36fe029c342
Nishit <nishitranjan41@gmail.com>
2022-10-07 17:52:37 +0530

    generte random digit and add

--- BasicsProgramming.js
+++ BasicsProgramming.js
@@ -2,7 +2,10 @@ let a = 10;// sequence pratice program
 let b = 20;
 let c = a + b;
 console.log(c);
-let x = Math.floor(Math.random()*10);//RandomFunctiionTogetSingleDigitNumber
+let x = Math.floor(Math.random()*10%6+1);//RandomFunctiionTogetSingleDigitNumber
 console.log(x);
 let y = Math.floor(Math.random()*10%6+1);
 console.log(y);
+console.log(x+y);
+
+
